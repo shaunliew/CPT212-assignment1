9d5b33251abf3f06d0072bc436145ac2b2937833
JacksonLiew101 <58961556+JacksonLiew101@users.noreply.github.com>
2022-05-16 02:38:19 +0800

    Add radixSort.java and start brainstorm for the algorithm

---
+++ src/radixSort.java
@@ -0,0 +1,11 @@
+// a) sorting words instead of numbers
+// b) words of different length
+// based on lecturer start from back to front
+
+
+public class radixSort {
+
+    static void radix_sort(){
+
+    }
+}
