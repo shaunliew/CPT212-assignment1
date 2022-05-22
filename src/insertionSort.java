1144fc6313fc1bd64c8b2ade049a98aff6dda8fd
JacksonLiew101 <58961556+JacksonLiew101@users.noreply.github.com>
2022-05-22 18:11:40 +0800

    Removed unused library

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -1,10 +1,6 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Vector;
-
 
 public class insertionSort {
     private insertionSort() {
